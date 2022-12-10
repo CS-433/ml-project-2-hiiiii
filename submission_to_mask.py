f5e0d6ca763d1cea80a745885c0c479cf356388d
unknown <faivre.bast@gmail.com>
2022-12-10 18:19:11 +0100

    fix: unet back to normal

--- submission_to_mask.py
+++ submission_to_mask.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python
 import os
 import sys
-import Image
+from PIL import Image
 import math
 import matplotlib.image as mpimg
 import numpy as np
 
-label_file = 'dummy_submission.csv'
+label_file = 'submission.csv'
 
 h = 16
 w = h
@@ -45,10 +45,10 @@ def reconstruct_from_labels(image_id):
 
         im[j:je, i:ie] = binary_to_uint8(adata)
 
-    Image.fromarray(im).save('prediction_' + '%.3d' % image_id + '.png')
+    Image.fromarray(im).save('test/prediction_' + '%.3d' % image_id + '.png')
 
     return im
 
-for i in range(1, 5):
+for i in range(1, 51):
     reconstruct_from_labels(i)
    
